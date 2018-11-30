1a26c4ff43edcf52d7087851926502e7a8e21433
karolinadrobotowicz <karolina.drobotowicz@gmail.com>
2018-11-30 20:47:56 +0100

    added version that should work with git

---
+++ Android_App_corrected/app/src/main/java/com/example/karolinadrobotowicz/mobiledev/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.karolinadrobotowicz.mobiledev;
+
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void notDeveloped(View view){
+        Snackbar.make(findViewById(R.id.coordinatorLayout),
+                R.string.notdeveloped, Snackbar.LENGTH_SHORT).show();
+    }
+
+    public void goToCommunity(View view) {
+        Intent intent = new Intent(this, communityscreen.class);
+        startActivity(intent);
+    }
+
+}
