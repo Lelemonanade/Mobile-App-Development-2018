dc98982d8e06d5bbb677b25a68393fc4489a208e
karolinadrobotowicz <karolina.drobotowicz@gmail.com>
2018-12-03 18:03:30 +0200

    changed names

--- Android_App_corrected/app/src/main/java/com/example/karolinadrobotowicz/mobiledev/communityscreen.java
+++ Android_App_corrected/app/src/main/java/com/example/karolinadrobotowicz/mobiledev/communityscreen.java
@@ -1,16 +1,14 @@
 package com.example.karolinadrobotowicz.mobiledev;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class communityscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.communityscreen);
+        setContentView(R.layout.activity_community);
     }
 
 
