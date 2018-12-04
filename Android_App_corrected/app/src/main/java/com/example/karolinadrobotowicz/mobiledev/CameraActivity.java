8f7e1578b646057a0a591290ffc554e7bee633d2
Lena Hegemann <s8lehege@stud.uni-saarland.de>
2018-12-04 11:26:08 +0100

    started fake camera activity

---
+++ Android_App_corrected/app/src/main/java/com/example/karolinadrobotowicz/mobiledev/CameraActivity.java
@@ -0,0 +1,44 @@
+package com.example.karolinadrobotowicz.mobiledev;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class CameraActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_back:
+                    mTextMessage.setText(R.string.title_back);
+                    return true;
+                case R.id.navigation_take_picture:
+                    mTextMessage.setText(R.string.title_take_picture);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_next);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
