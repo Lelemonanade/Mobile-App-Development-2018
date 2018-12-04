81f81b3a5ebaa1a3f9368e4c9ca2195406b94c08
Lena Hegemann <s8lehege@stud.uni-saarland.de>
2018-12-04 10:26:40 +0100

    reading strings and shortcut to add description view

--- Android_App_corrected/app/src/main/java/com/example/karolinadrobotowicz/mobiledev/MainActivity.java
+++ Android_App_corrected/app/src/main/java/com/example/karolinadrobotowicz/mobiledev/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,7 +21,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToCommunity(View view) {
+        Log.d("switch", "called method shortcut go to community");
+
         Intent intent = new Intent(this, CommunityActivity.class);
+        startActivity(intent);
+    }
+
+    public void shortcutAddDescription(View view){
+        //TODO remove
+        Log.d("switch", "called method shortcut add description");
+        Intent intent = new Intent(this, AddDescription.class);
         startActivity(intent);
     }
 
