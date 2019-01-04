6655921fcdcaccf04f44ef0b64f2f049e0edc7d9
karolinadrobotowicz <karolina.drobotowicz@gmail.com>
2019-01-04 19:10:08 +0200

    messages of option not delevoped added (also learn option from menu fixed)

--- Android_App_corrected/app/src/main/java/com/example/karolinadrobotowicz/mobiledev/CommunityActivity.java
+++ Android_App_corrected/app/src/main/java/com/example/karolinadrobotowicz/mobiledev/CommunityActivity.java
@@ -3,6 +3,7 @@ package com.example.karolinadrobotowicz.mobiledev;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -168,6 +169,11 @@ public class CommunityActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+
+    public void notDeveloped(View view){
+        Snackbar.make(findViewById(R.id.coordinatorLayout),
+                R.string.notdeveloped, Snackbar.LENGTH_SHORT).show();
+    }
 
     /*
     @Override
